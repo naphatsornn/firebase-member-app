fa758091d9b97c91102c5d747cddcbb9fd2fbffc
naphatsornn <naphatsorn.ki@ku.th>
2025-06-17 21:45:00 +0700

    login

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,2 @@
 // pages/index.js
-// pages/index.js
 export { default } from "./login";
-
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
-export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/login");
-  }, []);
-
-  return null;
-}
