ff30947813adb4ee2d6d2e64554bcc8e901383fb
naphatsornn <naphatsorn.ki@ku.th>
2025-06-19 12:26:35 +0700

    fix: adjust token extraction and logging in protected.js

--- pages/api/protected.js
+++ pages/api/protected.js
@@ -2,18 +2,13 @@ export default function handler(req, res) {
   const headers = req.headers;
 
   const token =
-    headers["tmn-access-token"] ||
+    headers["tmn-access-token"] ||  // ✅ token อยู่ตรงนี้
     headers["x-access-token"] ||
     (headers["authorization"]?.startsWith("Bearer ")
       ? headers["authorization"].split("Bearer ")[1]
       : null);
-      console.log("🔥 Token from header:", token);
-  
-      console.log("🔥 Raw Token Candidates:", {
-    "tmn-access-token": headers["tmn-access-token"],
-    "x-access-token": headers["x-access-token"],
-    authorization: headers["authorization"],
-  });
+
+  console.log("🔥 Token from header:", token);
 
   return res.status(token ? 200 : 401).json({
     message: token ? "✅ Token received" : "❌ Token not found",
